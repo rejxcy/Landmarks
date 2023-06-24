8d7bae861b9ad4f4cfa1e432984112d41d4c7c04
rejxcy <rejxcy0903@gmail.com>
2023-06-24 10:47:36 +0800

    init project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 胡志龍 on 2023/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
